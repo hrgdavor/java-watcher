05336542d8de134830397f842ef6db1f8199ebf2
hrgdavor <hrgdavor@gmail.com>
2016-11-29 14:40:27 +0100

    generated hash FileChangeEntry

--- src/main/java/hr/hrg/javawatcher/FileChangeEntry.java
+++ src/main/java/hr/hrg/javawatcher/FileChangeEntry.java
@@ -38,4 +38,38 @@ public class FileChangeEntry<T extends FileMatcher> {
 	public FileChangeType getType() {
 		return type;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((file == null) ? 0 : file.hashCode());
+		result = prime * result + ((matcher == null) ? 0 : matcher.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FileChangeEntry other = (FileChangeEntry) obj;
+		if (file == null) {
+			if (other.file != null)
+				return false;
+		} else if (!file.equals(other.file))
+			return false;
+		if (matcher == null) {
+			if (other.matcher != null)
+				return false;
+		} else if (!matcher.equals(other.matcher))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
 }
